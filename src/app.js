f8d41d40c9c301dcdb22c1e9d5ec3e3345f9d993
kamakship18 <kamakshipandoh8@gmail.com>
2024-01-01 01:12:42 +0530

    completed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 // Progression 1: Create special addition
-
 // function specialAddition(){}
-
+const specialAddition = (number1, number2) => {
+    return number1 < 0 || number2 < 0
+      ? "Negative numbers are not allowed"
+      : number1 - number2;
+  };
 // Progression 2: Create simple division
-
-// Progression 3: Create the special calculator
+const simpleDivision = (number1, number2) => {
+    return number1 < 0 || number2 < 0
+      ? "Negative numbers are not allowed"
+      : number1 < number2
+      ? "Cannot divide a smaller number by a larger number"
+      : number2 === 0
+      ? "Not a number"
+      : number1 / number2;
+}
+  // Progression 3: Create the special calculator
+  const specialCalculator = (number1, number2, operation) => {
+    return operation(number1, number2);
+  };
+  
